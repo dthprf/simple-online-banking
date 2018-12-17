d976c2d60f4a5923d2233863815a8a6b7c67ecc2
Bart <bartpetka@gmail.com>
2018-12-17 12:32:25 +0100

    Implement PersonalAccountRepositiory

---
+++ src/main/java/com/simplebanking/sob/Repository/PersonalAccountRepository.java
@@ -0,0 +1,7 @@
+package com.simplebanking.sob.Repository;
+
+import com.simplebanking.sob.Model.PersonalAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonalAccountRepository extends JpaRepository <PersonalAccount, Long> {
+}
