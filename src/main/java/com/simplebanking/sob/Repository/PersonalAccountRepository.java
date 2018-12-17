4c526cf7bf9e6796b9996408c33e5857ae4a94b5
Bart <bartpetka@gmail.com>
2018-12-18 00:57:47 +0100

    Implement getPersonalAccountByAccountId

--- src/main/java/com/simplebanking/sob/Repository/PersonalAccountRepository.java
+++ src/main/java/com/simplebanking/sob/Repository/PersonalAccountRepository.java
@@ -3,5 +3,7 @@ package com.simplebanking.sob.Repository;
 import com.simplebanking.sob.Model.PersonalAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PersonalAccountRepository extends JpaRepository <PersonalAccount, Long> {
+public interface PersonalAccountRepository extends JpaRepository<PersonalAccount, Long> {
+
+    PersonalAccount getPersonalAccountByAccountId(Long accountId);
 }
