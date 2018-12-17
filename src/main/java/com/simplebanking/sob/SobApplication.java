31e206c7cb27e68e7fff98a27818e2e7808a8b12
Bart <bartpetka@gmail.com>
2018-12-17 12:04:23 +0100

    Prepare project structure

---
+++ src/main/java/com/simplebanking/sob/SobApplication.java
@@ -0,0 +1,14 @@
+package com.simplebanking.sob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SobApplication.class, args);
+	}
+
+}
+
