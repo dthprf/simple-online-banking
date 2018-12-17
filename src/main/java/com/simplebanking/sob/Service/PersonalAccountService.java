58f46eebb44e1686152ad4d32f45976ce1f575be
Bart <bartpetka@gmail.com>
2018-12-17 12:48:31 +0100

    Implement personalAccountService interface

---
+++ src/main/java/com/simplebanking/sob/Service/PersonalAccountService.java
@@ -0,0 +1,11 @@
+package com.simplebanking.sob.Service;
+
+import com.simplebanking.sob.Model.PersonalAccount;
+
+import java.math.BigDecimal;
+
+public interface PersonalAccountService {
+    PersonalAccount createPersonalAccount(Long customerId, PersonalAccount personalAccount);
+    void addFounds(Integer accountNumber, BigDecimal value);
+    void removeFounds(Integer accountNumber, BigDecimal value);
+}
