1278f7713abe2ac5a2cdf2c184b72b9fdf1480d8
Bart <bartpetka@gmail.com>
2018-12-18 00:49:30 +0100

    Update Accountable with addTransaction, getBalance method signatures

--- src/main/java/com/simplebanking/sob/Model/Accountable.java
+++ src/main/java/com/simplebanking/sob/Model/Accountable.java
@@ -3,6 +3,12 @@ package com.simplebanking.sob.Model;
 import java.math.BigDecimal;
 
 public interface Accountable {
+    
+    BigDecimal getBalance();
+
+    void addTransaction(Operationable transfer);
+
     void addFounds(BigDecimal value);
+
     void removeFounds(BigDecimal value);
 }
