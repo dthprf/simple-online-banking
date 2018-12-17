ef4da5161ec97bb86e6e5144ba0722feeb0ed4d1
Bart <bartpetka@gmail.com>
2018-12-17 12:31:08 +0100

    Add customerRepository

---
+++ src/main/java/com/simplebanking/sob/Repository/CustomerRepositiory.java
@@ -0,0 +1,7 @@
+package com.simplebanking.sob.Repository;
+
+import com.simplebanking.sob.Model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepositiory extends JpaRepository <Customer, Long> {
+}
