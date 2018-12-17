7d58bf41718f2433591e0b3ba3fc1578b9db6474
Bart <bartpetka@gmail.com>
2018-12-17 12:19:09 +0100

    Add getters/setters

--- src/main/java/com/simplebanking/sob/Model/CustomerPersonalData.java
+++ src/main/java/com/simplebanking/sob/Model/CustomerPersonalData.java
@@ -26,5 +26,39 @@ public class CustomerPersonalData {
         this.dateOfBirth = dateOfBirth;
     }
 
+    public Long getUserDataId() {
+        return userDataId;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 }
