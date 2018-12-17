836a872aff8b86269b57620190e418a7c681f4bd
Bart <bartpetka@gmail.com>
2018-12-17 12:21:41 +0100

    Implement MobilePhoneNumber model

---
+++ src/main/java/com/simplebanking/sob/Model/MobilePhoneNumber.java
@@ -0,0 +1,44 @@
+package com.simplebanking.sob.Model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "contact_number")
+public class MobilePhoneNumber {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long numberId;
+    private int areaCode;
+    private int number;
+
+    protected MobilePhoneNumber() {}
+
+    public MobilePhoneNumber(int areaCode, int number) {
+        this.areaCode = areaCode;
+        this.number = number;
+    }
+
+    public Long getNumberId() {
+        return numberId;
+    }
+
+    public void setNumberId(Long numberId) {
+        this.numberId = numberId;
+    }
+
+    public int getAreaCode() {
+        return areaCode;
+    }
+
+    public void setAreaCode(int areaCode) {
+        this.areaCode = areaCode;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
