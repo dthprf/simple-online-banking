e69d21d51cf27d7320df4676869f38d1e3f17ab4
Bart <bartpetka@gmail.com>
2018-12-18 01:26:43 +0100

    Implement createInternalTransfer method

--- src/main/java/com/simplebanking/sob/Controller/TransferController.java
+++ src/main/java/com/simplebanking/sob/Controller/TransferController.java
@@ -13,3 +13,10 @@ public class TransferController {
 
     @Autowired
     TransferService transferService;
+
+    @PostMapping("accounts/{accountId}/transfers")
+    public TransferImpl createInternalTransfer(@PathVariable Long accountId,
+                                       @Valid @RequestBody TransferImpl transferImpl) {
+        return transferService.saveTransfer(transferImpl, accountId);
+    }
+
