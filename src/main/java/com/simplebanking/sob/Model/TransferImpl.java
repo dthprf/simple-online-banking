97f16b4cebcd52a7318cb4adbd23850bd3ecd3ef
Bart <bartpetka@gmail.com>
2018-12-19 21:35:29 +0100

    Add jsonfilter annotations

--- src/main/java/com/simplebanking/sob/Model/TransferImpl.java
+++ src/main/java/com/simplebanking/sob/Model/TransferImpl.java
@@ -1,28 +1,38 @@
 package com.simplebanking.sob.Model;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import javax.persistence.*;
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "transfer")
-public class TransferImpl implements Transfer {
+@JsonFilter("transferFilter")
+public class TransferImpl implements Transfer, Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("id")
     private Long transferId;
 
     @ManyToOne(targetEntity = PersonalAccount.class)
     @JoinColumn(name = "target_account_id")
+    @JsonProperty("target")
     private Accountable targetAccount;
 
     @ManyToOne(targetEntity = PersonalAccount.class)
     @JoinColumn(name = "source_account_id")
+    @JsonProperty("source")
     private Accountable sourceAccount;
     private BigDecimal value;
 
     @Enumerated(EnumType.STRING)
+    @JsonProperty("status")
     private TransactionStatus transactionStatus;
 
     @Enumerated(EnumType.STRING)
+    @JsonProperty("type")
     private TransferType transferType;
 
     protected TransferImpl() {
