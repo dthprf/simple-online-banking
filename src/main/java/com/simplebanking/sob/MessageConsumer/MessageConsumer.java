a4cd9e39fc2fe35d974a9b5226e5fd0ba5ddc16b
Bart <bartpetka@gmail.com>
2018-12-21 20:57:49 +0100

    Change method signature getRouteName - > getRouteKey

--- src/main/java/com/simplebanking/sob/MessageConsumer/MessageConsumer.java
+++ src/main/java/com/simplebanking/sob/MessageConsumer/MessageConsumer.java
@@ -2,11 +2,8 @@ package com.simplebanking.sob.MessageConsumer;
 
 import com.simplebanking.sob.Model.SOBMessage;
 
-import java.util.Queue;
-
 public interface MessageConsumer {
     void processMessage(SOBMessage message);
-    void setQueue(Queue<SOBMessage> queue);
     void enqueueMessage(SOBMessage message);
-    String getRouteName();
+    String getRouteKey();
 }
