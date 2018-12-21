e817d641b4eb56202d0cab1af5ac06dd5a5a0dde
Bart <bartpetka@gmail.com>
2018-12-21 20:58:16 +0100

    Change getRouteName method signature

--- src/main/java/com/simplebanking/sob/Exchange/Exchange.java
+++ src/main/java/com/simplebanking/sob/Exchange/Exchange.java
@@ -3,17 +3,18 @@ package com.simplebanking.sob.Exchange;
 import com.simplebanking.sob.MessageConsumer.MessageConsumer;
 import com.simplebanking.sob.Model.SOBMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-@Service
+@Component
+//@Scope(value = "prototype")
 public class Exchange {
 
     private Map<String, MessageConsumer> routeMap = new ConcurrentHashMap<>();
@@ -21,7 +22,7 @@ public class Exchange {
     @Autowired
     public void setRouteMap(Set<MessageConsumer> consumers) {
         this.routeMap = consumers.stream()
-        .collect(Collectors.toMap(MessageConsumer::getRouteName, Function.identity()));
+                .collect(Collectors.toMap(MessageConsumer::getRouteKey, Function.identity()));
     }
 
     public Map<String, MessageConsumer> getRouteMap() {
