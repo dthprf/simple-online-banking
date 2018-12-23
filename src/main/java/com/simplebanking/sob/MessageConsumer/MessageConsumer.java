62b2cac171e5588ffadd38b5602393a80fd0d717
Bart <bartpetka@gmail.com>
2018-12-23 19:31:29 +0100

    Change routeKey type to enum

--- src/main/java/com/simplebanking/sob/MessageConsumer/MessageConsumer.java
+++ src/main/java/com/simplebanking/sob/MessageConsumer/MessageConsumer.java
@@ -1,10 +1,11 @@
 package com.simplebanking.sob.MessageConsumer;
 
+import com.simplebanking.sob.Constants.RouteKey;
 import com.simplebanking.sob.Model.SOBMessage;
 
 public interface MessageConsumer {
     void processMessage(SOBMessage message);
     void enqueueMessage(SOBMessage message);
-    String getRouteKey();
+    RouteKey getRouteKey();
     void startConsumer();
 }
