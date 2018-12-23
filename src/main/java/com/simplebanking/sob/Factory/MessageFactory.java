f3aad3fc8c7b261197333f43242a515842b7b793
Bart <bartpetka@gmail.com>
2018-12-23 19:25:43 +0100

    Add params field to Message model

--- src/main/java/com/simplebanking/sob/Factory/MessageFactory.java
+++ src/main/java/com/simplebanking/sob/Factory/MessageFactory.java
@@ -1,20 +1,27 @@
 package com.simplebanking.sob.Factory;
 
+import com.simplebanking.sob.Constants.MethodType;
+import com.simplebanking.sob.Constants.RouteKey;
 import com.simplebanking.sob.Model.SOBMessage;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+import java.util.Set;
 
 @Component
 @Scope(value = "prototype")
 public class MessageFactory {
-    public SOBMessage createMessage(String method, String route, Object requestBody, Object deferredResult) {
-        return new SOBMessage(deferredResult, requestBody, method, route, null);
+
+    public SOBMessage createMessage(MethodType method, RouteKey route, Object requestBody, Object deferredResult) {
+        return new SOBMessage(deferredResult, requestBody, method, route, null, null);
     }
 
-    public SOBMessage createMessage(String method, String route, Object requestBody,
+    public SOBMessage createMessage(MethodType method, RouteKey route, Object requestBody,
                                     Object deferredResult, HashMap<String, Long> params) {
-        return new SOBMessage(deferredResult, requestBody, method, route, params);
+        return new SOBMessage(deferredResult, requestBody, method, route, params, null);
+    }
+
     public SOBMessage createMessage(MethodType method, RouteKey route, Object requestBody,
                                     Object deferredResult, HashMap<String, Long> params, Set<String> filterParams) {
         return new SOBMessage(deferredResult, requestBody, method, route, params, filterParams);
