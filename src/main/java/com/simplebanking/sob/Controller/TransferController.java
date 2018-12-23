1d75fd4610e3b88519bc0f2942674a9945f623c1
Bart <bartpetka@gmail.com>
2018-12-23 19:31:12 +0100

    Rebuild transfer controller -> message producer

--- src/main/java/com/simplebanking/sob/Controller/TransferController.java
+++ src/main/java/com/simplebanking/sob/Controller/TransferController.java
@@ -3,65 +3,84 @@ package com.simplebanking.sob.Controller;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.simplebanking.sob.Constants.MethodType;
+import com.simplebanking.sob.Constants.RouteKey;
+import com.simplebanking.sob.Exchange.Exchange;
+import com.simplebanking.sob.Factory.MessageFactory;
+import com.simplebanking.sob.Model.SOBMessage;
 import com.simplebanking.sob.Model.Transfer;
 import com.simplebanking.sob.Model.TransferImpl;
 import com.simplebanking.sob.Service.TransferService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
 @RestController
 public class TransferController {
 
+    private static final String TARGET_ACCOUNT_ID = "targetAccountId";
+    private static final String CUSTOMER_ACCOUNT = "sourceAccountId";
+
     @Autowired
     TransferService transferService;
 
+    @Autowired
+    Exchange exchange;
+
+    @Autowired
+    MessageFactory messageFactory;
+
     @PostMapping("accounts/{accountId}/transfers")
-    public MappingJacksonValue createInternalTransfer(@PathVariable Long accountId,
+    public DeferredResult<MappingJacksonValue> createInternalTransfer(@PathVariable Long accountId,
                                                       @Valid @RequestBody TransferImpl transferImpl) {
 
-        Transfer deputedTransfer = transferService.saveTransfer(transferImpl, accountId);
+        HashMap<String, Long> params = new HashMap<>();
+        params.put(CUSTOMER_ACCOUNT, accountId);
+        DeferredResult<MappingJacksonValue> response = new DeferredResult<>();
 
-        return serializeFullTransfer(deputedTransfer);
+        SOBMessage message = messageFactory.createMessage(MethodType.POST, RouteKey.TRANSFERS, transferImpl,
+                response, params);
+
+        exchange.routeMessage(message);
+
+        return response;
     }
 
     @PostMapping("accounts/{sourceAccountId}/transfers/{targetAccountId}")
-    public MappingJacksonValue createExternalTransfer(@PathVariable Long sourceAccountId, @PathVariable Long targetAccountId,
+    public DeferredResult<MappingJacksonValue> createExternalTransfer(@PathVariable Long sourceAccountId, @PathVariable Long targetAccountId,
                                                       @Valid @RequestBody TransferImpl transferImpl) {
+        HashMap<String, Long> params = new HashMap<>();
+        params.put(CUSTOMER_ACCOUNT, sourceAccountId);
+        params.put(TARGET_ACCOUNT_ID, targetAccountId);
+        DeferredResult<MappingJacksonValue> response = new DeferredResult<>();
 
-        Transfer deputedTransfer = transferService.saveTransfer(transferImpl, sourceAccountId, targetAccountId);
+        SOBMessage message = messageFactory.createMessage(MethodType.POST, RouteKey.TRANSFERS, transferImpl,
+                response, params);
 
-        return serializeFullTransfer(deputedTransfer);
+        exchange.routeMessage(message);
+
+        return response;
     }
 
     @GetMapping("accounts/{accountId}/transfers")
-    public MappingJacksonValue getTransactions(@RequestParam Set<String> fields,
-                                               @PathVariable Long accountId) {
+    public DeferredResult<MappingJacksonValue> getTransactions(@RequestParam Set<String> fields,
+                                                              @PathVariable Long accountId) {
 
-        List<TransferImpl> transfers = transferService.getTransactions(accountId);
+        HashMap<String, Long> params = new HashMap<>();
+        params.put(CUSTOMER_ACCOUNT, accountId);
+        DeferredResult<MappingJacksonValue> response = new DeferredResult<>();
 
-        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(transfers);
+        SOBMessage message = messageFactory.createMessage(MethodType.GET, RouteKey.TRANSFERS,
+                null, response, params, fields);
 
-        FilterProvider filters = new SimpleFilterProvider()
-                .addFilter("transferFilter", SimpleBeanPropertyFilter.filterOutAllExcept(fields));
+        exchange.routeMessage(message);
 
-        mappingJacksonValue.setFilters(filters);
-
-        return mappingJacksonValue;
-    }
-
-    private MappingJacksonValue serializeFullTransfer(Transfer transfer) {
-        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(transfer);
-
-        FilterProvider filters = new SimpleFilterProvider()
-                .addFilter("transferFilter", SimpleBeanPropertyFilter.serializeAll());
-
-        mappingJacksonValue.setFilters(filters);
-
-        return mappingJacksonValue;
+        return response;
     }
 }
