c210ddf8de7cdc46cc54f8919d8eeb75e1ca7b37
Bart <bartpetka@gmail.com>
2018-12-23 19:12:56 +0100

    Add @Scope / prototype

--- src/main/java/com/simplebanking/sob/Factory/MessageFactory.java
+++ src/main/java/com/simplebanking/sob/Factory/MessageFactory.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import java.util.HashMap;
 
 @Component
+@Scope(value = "prototype")
 public class MessageFactory {
     public SOBMessage createMessage(String method, String route, Object requestBody, Object deferredResult) {
         return new SOBMessage(deferredResult, requestBody, method, route, null);
