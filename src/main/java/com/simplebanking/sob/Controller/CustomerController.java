288dc9afb230746a1637f83850345d2c466f532d
Bart <bartpetka@gmail.com>
2018-12-23 19:24:19 +0100

    Rebuild Customer Controller to message producer

--- src/main/java/com/simplebanking/sob/Controller/CustomerController.java
+++ src/main/java/com/simplebanking/sob/Controller/CustomerController.java
@@ -1,11 +1,17 @@
 package com.simplebanking.sob.Controller;
 
+import com.simplebanking.sob.Constants.MethodType;
+import com.simplebanking.sob.Constants.RouteKey;
+import com.simplebanking.sob.Exchange.Exchange;
+import com.simplebanking.sob.Factory.MessageFactory;
 import com.simplebanking.sob.Model.Customer;
+import com.simplebanking.sob.Model.SOBMessage;
 import com.simplebanking.sob.Service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import javax.validation.Valid;
 
@@ -13,10 +19,17 @@ import javax.validation.Valid;
 public class CustomerController {
 
     @Autowired
-    private CustomerService customerService;
+    private MessageFactory messageFactory;
+
+    @Autowired
+    private Exchange exchange;
 
     @PostMapping("/customers")
-    public Customer createCustomer(@Valid @RequestBody Customer customer) {
-        return customerService.createCustomer(customer);
+    public DeferredResult<Customer> createCustomer(@Valid @RequestBody Customer customer) {
+        DeferredResult<Customer> result = new DeferredResult<>();
+        SOBMessage message = messageFactory.createMessage(MethodType.POST, RouteKey.CUSTOMERS, customer, result);
+        exchange.routeMessage(message);
+
+        return result;
     }
 }
